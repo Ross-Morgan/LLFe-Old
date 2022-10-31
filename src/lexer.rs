e63303dcd270b8b7a42ea746cbd62065eba36ee8
Ross-Morgan <morgan.ross.512@gmail.com>
2022-10-31 01:34:09 +0000

    add safe file loading

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,1 +1,32 @@
-pub struct Lexer {}
+use std::fs;
+use std::io;
+
+use crate::token::Token;
+
+
+type Tokens = Vec<Token>;
+
+pub struct Lexer(pub(crate) String);
+
+
+impl Lexer {
+    pub fn lex_file(filename: &str) -> Tokens {
+        Self::try_lex_file(filename).expect("Failed to open file")
+    }
+
+    pub fn try_lex_file(filename: &str) -> Result<Tokens, io::Error> {
+        let source_contents = fs::read_to_string(filename)?;
+        let lexer = Lexer(source_contents);
+
+        Ok(lexer.lex())
+    }
+}
+
+
+impl Lexer {
+    pub fn lex(&self) -> Tokens {
+        //TODO Implement lexer
+
+        vec![]
+    }
+}
