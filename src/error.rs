805e01bc65f5ab1f43f69c4aca88be8acb118212
Ross-Morgan <morgan.ross.512@gmail.com>
2022-10-31 15:53:58 +0000

    add Default and Debug to LLFeError

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,22 @@
+use std::fmt::Debug;
+
+#[derive(Clone)]
 pub enum LLFeError {
+    ERROR(String),
     LEXER(String),
     PARSER(String),
     TRANSPILER(String),
+}
+
+
+impl Default for LLFeError {
+    fn default() -> Self {
+        Self::ERROR("".to_string())
+    }
+}
+
+impl Debug for LLFeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "")
+    }
 }
