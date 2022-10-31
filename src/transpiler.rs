a9695a853961ac900227a0c472089f9e4894658d
Ross-Morgan <morgan.ross.512@gmail.com>
2022-10-31 01:36:39 +0000

    create Transpiler struct

--- src/transpiler.rs
+++ src/transpiler.rs
@@ -1,1 +1,11 @@
-pub struct Transpiler {}
+use crate::ast;
+
+pub struct Transpiler(pub(crate) ast::AST);
+
+
+impl Transpiler {
+    pub fn transpile(&self) -> String {
+        //TODO Implement transpiler
+        String::new()
+    }
+}
