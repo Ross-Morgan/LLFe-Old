aa06cfc6edaba65353a78a68dc8c212e3c79d716
Ross-Morgan <morgan.ross.512@gmail.com>
2022-10-31 01:34:18 +0000

    extend prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,33 @@
+pub mod error;
 pub mod lexer;
 pub mod parser;
+pub mod token;
 pub mod transpiler;
 
-pub mod prelude {
-    use super::*;
+pub mod ast;
 
-    pub use lexer::Lexer;
-    pub use parser::Parser;
-    pub use transpiler::Transpiler;
+pub mod prelude {
+    pub use super::error::LLFeError;
+    pub use super::lexer::Lexer;
+    pub use super::parser::Parser;
+    pub use super::transpiler::Transpiler;
+
+    pub use super::lex_parse_transpile;
+}
+
+
+pub fn lex_parse_transpile(source: String) -> Result<(), error::LLFeError> {
+    let lexer = lexer::Lexer(source);
+
+    let tokens = lexer.lex();
+
+    let parser = parser::Parser(tokens);
+
+    let ast = parser.parse();
+
+    let transpiler = transpiler::Transpiler(ast);
+
+    transpiler.transpile();
+
+    Ok(())
 }
