d6e135ce9cbe4df7e86d3394a1027ed9249865e1
Ross-Morgan <morgan.ross.512@gmail.com>
2022-11-13 23:39:26 +0000

    add to main

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,17 @@ use llfe::prelude::*;
 fn main() {
     let source = load_file("sample/source.llfe", true);
 
-    if let Err(e) = source {
-        panic!("{e:?}");
-    }
-
-    let lexer = Lexer(source.unwrap());
+    let lexer = match source {
+        Ok(s) => Lexer(s),
+        Err(e) => panic!("{:?}", e),
+    };
 
     let mut contents = vec![];
 
-    lexer.find_section_contents(&vec![], &mut contents);
+    let e = lexer.find_section_contents(&vec![], &mut contents);
 
     println!("---");
+    println!("{:?}", contents);
+    println!("{:?}", e);
     println!("---");
 }
